714b26c5088b7ea89b53e088396be243d8cbcd1f
Mnyazi <mnyazi@192.168.1.3>
2017-07-20 13:37:24 +0300

    modified tests user tests

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -2,10 +2,11 @@ import unittest
 from app.users.users import User
 
 
-class TestUsers(unittest.TestCase):
-    def test_user(self, ):
+class TestUser(unittest.TestCase):
+    def test_is_type(self):
         new_user = User('mary poppins', 'mpoppins@test.com', 'password')
-        self.assertIsInstance(new_user, User)                
+        self.assertIsInstance(new_user, User)
+
 
 if __name__ == '__main__':
     unittest.main()
