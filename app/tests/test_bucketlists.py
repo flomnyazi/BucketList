c1ae6a93e8e83ec1a0957936cb6b74ce1d928b16
Mnyazi <mnyazi@192.168.1.3>
2017-07-20 13:40:52 +0300

    modified tests bucketlist test

--- app/tests/test_bucketlists.py
+++ app/tests/test_bucketlists.py
@@ -3,16 +3,11 @@ from app.bucketlists.bucketlists import BucketList
 from app.items.items import Item
 
 
-class TestBucketLists(unittest.TestCase):
-    def BucketList(self):
-        self.bucket_list = BucketList('BucketList 1',)
+class TestBucketList(unittest.TestCase):
+    def test_is_type(self):
+        new_bucketlist = BucketList('BucketList 1')
+        self.assertIsInstance(new_bucketlist, BucketList)
 
-    def test_create_activity(self):
-        new_activity = self.bucket_list.create_activity('Learn to Scuba dive')
-        self.assertIsInstance(new_activity, Activity)
 
-    def test_view_activity(self):
-        pass
-
-    def test_edit_activity(self):
-        pass
+if __name__ == '__main__':
+    unittest.main()
