9f171f18711806607a2c470d362e24b227bce1be
Mnyazi <mnyazi@192.168.1.3>
2017-07-20 12:52:33 +0300

    modified tests

--- app/tests/test_bucketlists.py
+++ app/tests/test_bucketlists.py
@@ -1,7 +1,18 @@
-from unittest import TestCase
+import unittest
 from app.bucketlists.bucketlists import BucketList
 from app.items.items import Item
 
 
-class TestBucketLists(TestCase):
-    pass
+class TestBucketLists(unittest.TestCase):
+    def BucketList(self):
+        self.bucket_list = BucketList('BucketList 1',)
+
+    def test_create_activity(self):
+        new_activity = self.bucket_list.create_activity('Learn to Scuba dive')
+        self.assertIsInstance(new_activity, Activity)
+
+    def test_view_activity(self):
+        pass
+
+    def test_edit_activity(self):
+        pass
--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -3,9 +3,9 @@ from app.users.users import User
 
 
 class TestUsers(unittest.TestCase):
-    def test_user(self):
-        pass
-
+    def test_user(self, ):
+        new_user = User('mary poppins', 'mpoppins@test.com', 'password')
+        self.assertIsInstance(new_user, User)                
 
 if __name__ == '__main__':
     unittest.main()
