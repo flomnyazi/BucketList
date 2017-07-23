dfc4b22f4ee12cdfff4e71c1bf4af31cfbd053ff
Mnyazi <mnyazi@192.168.1.3>
2017-07-23 20:12:50 +0300

    modified files

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app.app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-#this script imports theapp variable from the app package and invokes the run method to start the server.
-# run this script to start the app
+# this script imports the app variable from the app package and invokes the run method to start the server.
+#  run this script to start the app
