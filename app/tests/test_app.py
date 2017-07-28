34c9b29929a6b83277318f570f3df8b809924c45
Mnyazi <mnyazi@192.168.1.7>
2017-07-28 07:51:50 +0300

    refactor tests for app

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -15,16 +15,23 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_a_user_can_login(self):
-        pass
+        response = self.client.post('/login', self.user1)
+        self.assertEqual(response.status_code, 200)
+        
 
     def test_a_user_can_logout(self):
-        pass
-
-    def testit_can_create_a_bucketlist(self):
-        pass
+        response = self.client.post('/logout', self.user1)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_can_create_a_bucketlist(self):
+        self.buckets.Bucketlists = {}
+        output = self.buckets.create('Bucketlist 1', 'Fashion','owner@gmail.com')
+        self.assertEqual(1,output, "Bucket successfully created")
+    
 
     def test_can_list_bucketlists(self):
-        pass
+        response = self.client.post('/mybucketlist', self.user1)
+        self.assertEqual(response.status_code, 200)
 
     def test_can_update_a_bucketlist(self):
         self
@@ -46,5 +53,3 @@ if __name__ == '__mian__':
     unittest.main
     
 
-if __name__ == '__main__':
-    unittest.main()
